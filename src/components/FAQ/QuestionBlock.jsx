a97007f5a1324ab97dd16c565ea66423f397524f
Frederick Pawelzik <frederickpawelzik@gmail.com>
2024-07-19 15:05:34 +0200

    faq dropdown fix4

--- src/components/FAQ/QuestionBlock.jsx
+++ src/components/FAQ/QuestionBlock.jsx
@@ -1,18 +1,23 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./QuestionBlock.css";
 
 const QuestionBlock = ({ data }) => {
   const [answerToggle, setAnswerToggle] = useState(false);
+  const answerRef = useRef(null);
+  let timeoutId;
 
   const handleToggle = () => {
-    const answerElement = document.querySelector(".answer");
+    const answerElement = answerRef.current;
+
     if (answerToggle) {
       answerElement.classList.remove("show_answer");
       answerElement.classList.add("hide_answer");
 
       // Use a timeout to set display: none after the animation completes
-      setTimeout(() => {
-        answerElement.style.display = "none";
+      timeoutId = setTimeout(() => {
+        if (answerElement) {
+          answerElement.style.display = "none";
+        }
       }, 400); // Match this duration with the closeanswer animation duration
     } else {
       answerElement.classList.remove("hide_answer");
@@ -22,6 +27,10 @@ const QuestionBlock = ({ data }) => {
 
     setAnswerToggle((prev) => !prev);
   };
+
+  useEffect(() => {
+    return () => clearTimeout(timeoutId); // Cleanup the timeout on component unmount
+  }, []);
 
   return (
     <div className="question_block">
@@ -35,7 +44,10 @@ const QuestionBlock = ({ data }) => {
             ></div>
           </div>
         </div>
-        <p className={`answer ${answerToggle ? "show_answer" : "hide_answer"}`}>
+        <p
+          ref={answerRef}
+          className={`answer ${answerToggle ? "show_answer" : "hide_answer"}`}
+        >
           {data.answer}
         </p>
       </div>
